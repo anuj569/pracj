a648c7393b9b0a319dd43f25989e794e798944bb
anujkumar pandey <anuj569@gmail.com>
2022-02-02 21:35:57 +0530

    enable error

--- prac15.php
+++ prac15.php
@@ -1,6 +1,6 @@
 <?php
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 $numbers = array(12,23,45,20,5,1025,6,34,17,9000,56,999);
 
 $final = 0;
